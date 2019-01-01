3127c3b506e9a44d1208b5893de7d2af154d0c5e
Certani <sttng1511@gmail.com>
2019-01-01 18:30:05 +0100

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,10 @@
-# content of test_sample.py
+# a minimal pytest example from pytest
+
+# this function will be tested
 def func(x):
     return x + 1
 
+# we assume if we enter 4 the return value will be 5
 def test_answer():
     assert func(4) == 5
 
